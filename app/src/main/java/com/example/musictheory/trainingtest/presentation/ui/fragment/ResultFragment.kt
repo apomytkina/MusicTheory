9d6afe201efe20676d5ea17656ca66fc60724bd3
Данияр <daniyar.baiganov@gmail.com>
2021-11-14 17:41:22 +0300

    - remade notes

--- app/src/main/java/com/example/musictheory/trainingtest/presentation/ui/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/musictheory/trainingtest/presentation/ui/fragment/ResultFragment.kt
@@ -1,7 +1,6 @@
 package com.example.musictheory.trainingtest.presentation.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -36,13 +35,11 @@ class ResultFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.result.observe(viewLifecycleOwner, { result ->
-                Log.d("xxx", "res $result")
             getTest(result)
         })
 
         arguments?.let {
             val id = it.getInt(ARG_ID_RESULT)
-            Log.d("xxx", "id $id")
             viewModel.getResultById(id)
         }
     }
