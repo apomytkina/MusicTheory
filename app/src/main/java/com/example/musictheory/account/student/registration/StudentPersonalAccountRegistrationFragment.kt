ec28649857864df66190946f951a51256d0904ad
apomytkina <annapoma90@mail,ru>
2021-12-01 16:49:40 +0300

    account fragments were added

---
+++ app/src/main/java/com/example/musictheory/account/student/registration/StudentPersonalAccountRegistrationFragment.kt
@@ -0,0 +1,22 @@
+package com.example.musictheory.account.student.registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.musictheory.R
+
+class StudentPersonalAccountRegistrationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_student_personal_account_registration,
+            container,
+            false
+        )
+    }
+}
